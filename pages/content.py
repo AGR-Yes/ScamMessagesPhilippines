8968491e367426b4266aa03a5baf7996f749a1d1
Anton Reyes <anton_reyes@dlsu.edu.ph>
2023-07-24 21:30:31 +0800

    Content template complete

--- pages/content.py
+++ pages/content.py
@@ -2,7 +2,7 @@ import dash
 from dash import Dash, dcc, html, Input, Output, dash_table, State
 import dash_bootstrap_components as dbc
 import plotly.express as px
-
+import plotly.graph_objects as go
 
 #---------------------------------#
 #Duplicate this as needed, but this is the template for the content 
@@ -17,13 +17,15 @@ layout = html.Div(
             children=[
                 html.Div([ 
                     
+                    html.Br(),
                     html.P('Title', style={
-                                "margin-top": "30px",
+                                "margin-top": "60px",
                                 'text-align': 'center', 
                                 'font-size': '50px',
                                 "font-family": "'Press Start 2P', display",
+                                "padding": "5px",
                                 }), 
-                    
+                    html.Br(),
                     html.P(
                             """
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida vel ipsum in luctus. Sed rhoncus sagittis tellus, vitae egestas orci. Mauris interdum iaculis eros et dignissim. Pellentesque commodo volutpat ex convallis malesuada. Donec vehicula tincidunt ante, eu dignissim nisl pharetra ac. Nulla nisi magna, hendrerit eu libero vitae, blandit interdum ipsum. Aenean quis viverra nibh.
@@ -42,15 +44,22 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida vel ipsu
                 ], 
         ),
         
-        
+        html.Br(),
         
 #Content Row 1        
         dbc.Row(
             children=[
                 
-                
-                
-                dbc.Col( #Left Column
+                html.Div([
+
+                    html.H2("Chart Title", style={
+                                "text-align": "right",
+                                "font-family": "'Press Start 2P', display",
+                                })
+                ]),
+
+#Left Column                
+                dbc.Col( 
                     html.Div([
 
                             html.Div(
@@ -69,97 +78,116 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida vel ipsu
                     width=5,  # Adjust the width of the first column
                 ),
 
-                dbc.Col( #Right Column
+#Right Column
+                dbc.Col( 
                     html.Div([
                         
                         html.Img(src='https://placehold.co/600X300', alt='Placeholder Image', 
-                                 style={"align": "right",})
+                                 style={"align": "right",
+                                        "position": "relative"})
 
                     ]),
                     width=7,  # Adjust the width of the second column
                     className="mt-4",
                 ),
-            ], style = {
-                        "width": "90%",
-                        "margin-left": "auto",
-                        "margin-right": "auto",}
+            ]
         ),
 
         html.Br(),
 
+
+#Content Row 2
         dbc.Row(
             children=[
-                dbc.Col(
-                    #buffer
-                    width=2
-                ),
-                dbc.Col(
-                    html.Div(
-                        [
-                            html.P(
-                                '''
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida vel ipsum in luctus. Sed rhoncus sagittis tellus, vitae egestas orci. Mauris interdum iaculis eros et dignissim. Pellentesque commodo volutpat ex convallis malesuada.
-                                '''
-                                ),
-                            
-                            html.Br(),
 
-                            html.Div(
-                                [
-                                html.H1('Test'),
-                                html.P(
-                                    '''
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida vel ipsum in luctus. Sed rhoncus sagittis tellus, vitae egestas orci. Mauris interdum iaculis eros et dignissim. Pellentesque commodo volutpat ex convallis malesuada.
-                                    '''
-                                )
-                            ], style = {"border": "1px black solid",
-                                        "border-radius": "10px",
-                                        "padding": "10px",
-                                        'whiteSpace': 'pre-wrap'}
-                            ),
-                        ], 
-                    ), width=4
-                ),
+#Left column
                 dbc.Col(
                     html.Div(
                         [
-                            html.Br(),
-                            html.Div(
-                                [
-                                html.H1('Test'),
-                                html.P(
-                                    '''
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida vel ipsum in luctus. Sed rhoncus sagittis tellus, vitae egestas orci. Mauris interdum iaculis eros et dignissim. Pellentesque commodo volutpat ex convallis malesuada.
-                                    '''
-                                )
-                            ], style = {"border": "1px black solid",
-                                        "border-radius": "10px",
-                                        "padding": "10px",
-                                        'whiteSpace': 'pre-wrap'}
-                            ),
+                           
+                            html.Img(src='https://placehold.co/500X350', alt='Placeholder Image', 
+                                 style={"align": "right",
+                                        "position": "fixed"}),
+
+
+                        ] 
+                    ), width=7
+                ),
+
+#Right column
+                dbc.Col(
+                    html.Div(
+                        [
 
                             html.Br(),
                             html.Div(
                                 [
-                                html.H1('Test'),
+
                                 html.P(
                                     '''
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida vel ipsum in luctus. Sed rhoncus sagittis tellus, vitae egestas orci. Mauris interdum iaculis eros et dignissim. Pellentesque commodo volutpat ex convallis malesuada.
                                     '''
                                 )
+
                             ], style = {"border": "1px black solid",
                                         "border-radius": "10px",
                                         "padding": "10px",
-                                        'whiteSpace': 'pre-wrap'}
+                                        'whiteSpace': 'pre-wrap'},                                
                             ),
+                            html.Br(),
+                            html.H1('Test')
+
                         ], 
-                    ), width=4
-                ),
-                dbc.Col(
-                    #buffer
-                    width=2
-                ),
+                    ), width=5
+                )
+            ], style = {
+                        "padding": "20px",
+                        "width": "80%",
+                        "margin-left": "auto",
+                        "margin-right": "auto",}
+
+        ),
+
+        html.Br(),
+
+
+#Content Row 3
+        dbc.Row(
+            children=[
+                html.Div([
+
+                    html.P(
+                        """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida vel ipsum in luctus. Sed rhoncus sagittis tellus, vitae egestas orci. Mauris interdum iaculis eros et dignissim. Pellentesque commodo volutpat ex convallis malesuada. Donec vehicula tincidunt ante, eu dignissim nisl pharetra ac. Nulla nisi magna, hendrerit eu libero vitae, blandit interdum ipsum. Aenean quis viverra nibh.
+                        """, style={
+                            "margin-top": "5px",
+                            'text-align': 'center', 
+                            'font-size': '12px',
+                            "font-family": "'Roboto', serif",
+                            "padding": "5px",
+                            #"width": "80%",
+                            "margin-left": "auto",
+                            "margin-right": "auto",
+                            }),
+                ])
             ]
-        )
+        ),
+
+        html.Br(),
+
+#Content Row 4
+        dbc.Row(
+            children=[
+                html.Div([
+
+                    html.Img(src='https://placehold.co/800X300', alt='Placeholder Image')
+
+                ], style={"text-align": "center",
+                          "padding": "20px"}
+            )
+        ])
+
     ]
 )
+
+#Continue as needed
